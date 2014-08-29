9c5bd1aa7f55dd99fa6df5bd7a6c7eb49ed90047
Bobby Boy <vova@localhost.localdomain>
2014-08-29 18:46:41 +0300

    Create custom size game board

--- table.lisp
+++ table.lisp
@@ -23,29 +23,47 @@
     (*standard-output* nil :prologue t :indent t)
     (:html :xmlns "http://www.w3.org/1999/xhtml" :xml\:lang "en" :lang "en"
            (:head (:meta :http-equiv "Content-Type" :content "text/html;charset=utf-8")
-                  (:title ,(format nil "~@[~A - ~]Test Site" title)))
+                  (:title ,(format nil "~@[~A - ~] Epidemia Test Site" title))
+		  (:link :type "text/css"
+			 :rel "stylesheet"
+			 :href "css/epidemia.css"))
            (:body ,@body))))
 
 (defmacro define-url-fn ((name) &body body)
   `(progn
      (defun ,name ()
        ,@body)
-     (push (create-prefix-dispatcher ,(format nil "/~(~a~).htm" name) ',name) *dispatch-table*)))
+     (push (create-prefix-dispatcher ,(format nil "/~(~a~).html" name) ',name) *dispatch-table*)))
 
 (define-easy-handler (test-page :uri "/") ()
-  (page-template (:title "Splash Page") (:p "Testing testing")))
+  (page-template (:title "Splash Page") 
+		 (:p "Go to new-game.html")))
 
+(define-url-fn (new-game)
+  (page-template (:title "New Game")
+		 (:p "Create new square game board")
+		 (:form :action "/game.html" :method "post"
+			(:p "Game board size"
+			(:input :type "number" 
+				:name "board_size" 
+				:value "15" 
+				:min "9" 
+				:max "21"))
+			(:p (:input :type "submit"
+				:value "Create"
+				:class "btn" )))))
    
-(define-url-fn (table)
-	       (page-template (:title "Table test") 
-			      (:table :border 0 :cellpadding 4
-				       (loop for i below 25 by 5
-						 do (htm
-						     (:tr :align "right"
-						      (loop for j from i below (+ i 5)
-							    do (htm
-								(:td :bgcolor (if (oddp j)
-										"white"
-										"black")
-								     (:img :src "/cell.png" ))))))))))
+(define-url-fn (game)
+	       (let ((size (parse-integer (parameter "board_size"))))
+		       (page-template (:title "Table test") 
+				      (:table :border 0 :cellpadding 0
+					       (loop for i below (* size size) by size
+							 do (htm
+							     (:tr :align "right"
+							      (loop for j from i below (+ i size)
+								    do (htm
+									(:td :bgcolor (if (oddp j)
+											"white"
+											"black")
+									     (:img :src "/cell.png" :onclick (ps-inline (alert "Hello, parenscript")))))))))))))
 
